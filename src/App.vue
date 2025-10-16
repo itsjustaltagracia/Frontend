3d595c18775937f8cdb317e31f48a98932bc3eef
Admin <itsjustaltagracia@gmail.com>
2025-10-16 14:05:08 -0400

    Actualizo con los últimos cambios

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Gestión de Ítems</h1>
+    <ItemsList />
+  </div>
+</template>
+
+<script>
+import ItemsList from './components/ItemsList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ItemsList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
